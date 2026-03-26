d51b11b1ceac989f9d492b63ecb3868924329d22
Khazin <175429319+KhazinAwan@users.noreply.github.com>
2026-03-26 19:47:35 +0500

    Add logic for operations and display result
    Created variables to store operands and operator
    Handled digits and operator inputs, inc =
    Implemented chained calculations for continuous operations
    Displaying result to show output

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let firstOperand = ""
+let operator = ""
+let secondOperand = ""
+
 function addition(num1 , num2) {
 
     return num1 + num2
@@ -24,13 +28,66 @@ function operate(num1 , num2 , operator) {
 
     if(operator == '-') return subtraction(num1, num2)
 
-    if(operator == '*') return multiplication(num1, num2)
+    if(operator == 'x') return multiplication(num1, num2)
 
-    if(operator == '/') return division(num1, num2)
+    if(operator == '÷') return division(num1, num2)
 
 }
 
-console.log(operate(10 , 5 , '+'))
+/*console.log(operate(10 , 5 , '+'))
 console.log(operate(10 , 5 , '-'))
 console.log(operate(10 , 5 , '*'))
-console.log(operate(10 , 5 , '/'))
+console.log(operate(10 , 5 , '/'))*/
+
+
+const digits = document.querySelector(".digits")
+const operations = document.querySelector(".operators")
+const display = document.querySelector(".display")
+
+digits.addEventListener("click" , (e) => {
+
+    if(e.target.tagName === "BUTTON"){
+
+        if(operator == "")  {
+
+            firstOperand += e.target.textContent
+            console.log(`${firstOperand}`)
+        }
+
+        else    {
+
+            secondOperand += e.target.textContent
+            console.log(`${secondOperand}`)
+
+        }
+    }
+})
+
+operations.addEventListener("click" , (e) => {
+
+    if(e.target.tagName === "BUTTON") {
+
+        if(e.target.textContent == "=" && secondOperand == "")  return
+
+        if(secondOperand == "") {
+
+            operator = e.target.textContent
+
+        }
+
+        else {
+
+            const result = operate(Number(firstOperand) , Number(secondOperand) , operator)
+            display.textContent = result
+
+            firstOperand = String(result)
+            secondOperand = ""
+
+            if(e.target.textContent != "=") {
+
+                operator = e.target.textContent
+
+            }
+        }
+    }
+})
